24235017056c14ba9a0be8772a5bfee04847ed40
ivandiFF <ivandi@fansfirst.ca>
2021-12-15 15:14:38 +0800

    added setup on bin folder & updated run test case

---
+++ test/run.testcases.js
@@ -0,0 +1,5 @@
+require("./createparkslot.test.js");
+require("./park.test.js");
+require("./leavepark.test.js");
+require("./parkingstatus.test.js");
+require("./registration_numbers_for_cars_with_colour.test.js");
